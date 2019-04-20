b552b258463300c4cd4f52cdcccb4eceab872164
Scott <scottwesleymusic@gmail.com>
2019-04-20 11:08:26 -0500

    fixes overflow issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
-console.log(123);
+const hamIcon = document.getElementById("hamburger-icon");
+const closeIcon = document.getElementById('close-icon');
+const mobileNav = document.getElementById('mobile-nav');
+const overlay = document.getElementById('overlay');
+
+
+function openNav(e) {
+    console.log('fuck');
+
+    overlay.style.display = 'block';
+    mobileNav.style.right = '0px';
+}
+
+function closeNav() {
+    console.log('fuck');
+    overlay.style.display = 'none';
+    mobileNav.style.right = '-250px';
+}
+
+
+
+hamIcon.addEventListener('click', openNav);
+closeIcon.addEventListener('click', closeNav);
+
+
+
+//took away header-text FLEX..so put that back in
+//commented out OVERLAY, MOBILE-NAV AND CLOSE ICON
+//commented out PHONE NAV in HTML
