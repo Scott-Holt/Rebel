48c2a957b234fe14519a769163476b1028d9bd7d
Scott <scottwesleymusic@gmail.com>
2019-05-30 15:00:59 -0500

    adds php contact form

---
+++ contact-form.php
@@ -0,0 +1,14 @@
+<?php
+    if(isset($_POST['submit'])){
+        $firstName = $_POST['firstname'];
+        $lastName = $_POST['lastname'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['comment'];
+
+        $mailTo = 'jholt@go2rebel.com';
+        $headers = 'From: '.$mailFrom;
+        $txt = 'You have recieved an email from '.$firstName.' '.$lastName.'.\n\n.'. $message;
+        mail($mailTo, $txt, $headers);
+        header('Location: index.php?mailsend');
+    }
+?>
